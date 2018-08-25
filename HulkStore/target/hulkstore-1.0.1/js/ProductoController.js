768287394f4ca9eba16c1af0662e2707e2a45b19
Jhoana Castillo <jhoanacastillo@MacBook-Air-de-Jhoana.local>
2018-08-25 15:59:59 -0500

    inicio de laspantallas

--- HulkStore/target/hulkstore-1.0.1/js/ProductoController.js
+++ HulkStore/target/hulkstore-1.0.1/js/ProductoController.js
@@ -3,15 +3,28 @@
 
 module.controller('ProductoCtrl', ['$scope', '$filter', '$http', function ($scope, $filter, $http) {
 
+        $scope.carrito = [];
+
         $scope.listarProductos = function () {
             $http.get('./webresources/Producto', {})
                     .success(function (data, status, headers, config) {
-                        $scope.productos = data;
+                        $scope.lista = data;
                     }).error(function (data, status, headers, config) {
                 alert('Error al consultar la informaci\xf3n, por favor intente m\xe1s tarde');
             });
         };
         $scope.listarProductos();
+
+        $scope.agregarProducto = function (producto) {
+            $scope.carrito.push(producto);
+        };
+
+        $scope.borrar = function (producto) {
+            var index = $scope.carrito.indexOf(producto);
+            if (index > -1) {
+                $scope.carrito.splice(index, 1);
+            }
+        }
 
 
     }]);
